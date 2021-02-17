a5dffb6aedcf93b888aecc848356d45f8d65c4d3
Nabanita Bag <ashley.makruz@gmail.com>
2021-02-17 19:44:03 +0530

    add supervillains to the arena

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
-console.log("hello, world");
-
 fs.copyFileSync("file1.txt", "file2.txt");
 
-console.log("hello, world");
+
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', �]
+
+var mySuperHero = superheroes.random();
+var mySuperVillain = supervillains.random();
+console.log(mySuperHero+" fights "+mySuperVillain);
